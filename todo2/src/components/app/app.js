865e770217d87a6a541887ab2b646eab2e044b92
Gergely Ószer <gergely_oszer@epam.com>
2021-03-10 00:49:36 +0100

    Home assigment: - title with counters - text filter - status filter

---
+++ todo2/src/components/app/app.js
@@ -0,0 +1,170 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
+
+import './app.css';
+
+export default class App extends Component {
+
+  getTitle = (list) => {
+    let totalCount = list.length;
+    let doneCount = list.filter((el) => el.done).length;
+    return (totalCount - doneCount) + ' more to do, ' + doneCount + ' done';
+  }
+
+  myTodos = [
+    this.createTodo('Drink Coffee'),
+    this.createTodo('Make Awesome App'),
+    this.createTodo('Have a lunch')
+  ];
+
+  state = {
+    todoData: this.myTodos,
+    title : this.getTitle(this.myTodos),
+    filterText: '',
+    statusFilter: 'All'
+  };
+
+  createTodo(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      visible: true,
+      id: nanoid(10)
+    };
+  }
+
+  onAdded = (label) => {
+    this.setState(({ todoData }) => {
+      const todo = this.createTodo(label);
+      let res = [...todoData, todo];
+      res = this.filterList(res, this.state.filterText, this.state.statusFilter);
+      return {
+        todoData: res,
+        title: this.getTitle(res),
+        filterText: this.state.filterText,
+        statusFilter: this.state.statusFilter
+      }
+    });
+  };
+
+  onDeleted = (id) => {
+    this.setState(({ todoData }) => {
+      const res = todoData.filter((el) => el.id !== id);
+      return {
+        todoData: res,
+        title: this.getTitle(res),
+        filterText: this.state.filterText,
+        statusFilter: this.state.statusFilter
+      };
+
+    });
+  };
+
+  toggleProperty(arr, prop, id) {
+    return arr.map((el) => {
+      if (el.id !== id) {
+        return el;
+      }
+
+      return {...el, [prop]: !el[prop] };
+    });
+  }
+
+  toggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      let res = this.toggleProperty(todoData, 'important', id);
+      return {
+        todoData: res,
+        title: this.getTitle(res),
+        filterText: this.state.filterText,
+        statusFilter: this.state.statusFilter
+      }
+    });
+  };
+
+  toggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      let res = this.toggleProperty(todoData, 'done', id);
+      return {
+        todoData: res,
+        title: this.getTitle(res),
+        filterText: this.state.filterText,
+        statusFilter: this.state.statusFilter
+      }
+    });
+  };
+
+  filterList = (list, filterText, statusFilter) => {
+    return list.map((el) => {
+      let visibleByStatus = false;
+      switch(statusFilter) {
+        case 'All':
+          visibleByStatus = true;
+          break;
+        case 'Active':
+          visibleByStatus = !el.done;
+          break;
+        case 'Done':
+          visibleByStatus = el.done;
+      }
+      let visibleByText = el.label.toUpperCase().includes(filterText.toUpperCase()) ;
+      let isVisible = visibleByStatus && visibleByText;
+      return {...el, ['visible']: isVisible };
+    });
+  };
+
+  onTextFilter = (filterText) => {
+    this.setState(({ todoData }) => {
+      let res = this.filterList(todoData, filterText, this.state.statusFilter);
+
+      return {
+        todoData: res,
+        title: this.state.title,
+        filterText: filterText,
+        statusFilter: this.state.statusFilter
+      };
+    });
+  }
+
+  onStatusFilter = (selected) => {
+    this.setState(({ todoData }) => {
+      let res = this.filterList(todoData, this.state.filterText, selected);
+
+      return {
+        todoData: res,
+        title: this.state.title,
+        filterText: this.state.filterText,
+        statusFilter: selected
+      }
+    });
+  }
+
+  render() {
+    const { todoData, title } = this.state;
+
+    return (
+      <div className="todo-app">
+        <AppHeader title={title} />
+        <div className="top-panel d-flex">
+          <SearchPanel onFilter={this.onTextFilter}  />
+          <ItemStatusFilter onFilter={this.onStatusFilter} />
+        </div>
+
+        <TodoList
+          todos={todoData}
+          onDeleted={this.onDeleted}
+          toggleImportant={this.toggleImportant}
+          toggleDone={this.toggleDone} />
+
+        <ItemAddForm onItemAdd={this.onAdded} />
+      </div>
+    );
+  }
+}
---
+++ todo2/src/components/search-panel/search-panel.js
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import './search-panel.css';
+
+export default class SearchPanel extends Component{
+
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    const { onFilter } = this.props;
+    const filterText = e.target.value;
+
+    this.setState({
+      label: filterText
+    });
+
+    onFilter(filterText);
+  };
+
+  render() {
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="type to search"
+               onChange={this.onLabelChange}
+               value={this.state.label}/>
+    );
+  }
+};
+
