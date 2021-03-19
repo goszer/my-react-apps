103f414e50f235d2320ba313196a7399f754d602
Gergely Ószer <gergely_oszer@epam.com>
2021-03-19 01:23:54 +0100

    Fix no trailer issue

--- tv-shows/src/screens/TvShowDetailsScreen/components/TvShowTrailer.js
+++ tv-shows/src/screens/TvShowDetailsScreen/components/TvShowTrailer.js
@@ -10,39 +10,45 @@ const TvShowTrailer = (id) => {
 
     const filterTrailer = () => {
         if (!isLoaded || error) {
-            return [];
+            return null;
         }
         const realTrailer = trailers.filter((t) =>
             t.type.toLowerCase().includes('trailer')
         );
+        if (!realTrailer[0]) {
+            return null;
+        }
         const videoKey = realTrailer[0].key;
         return videoKey;
     };
 
-    return (
-        <Modal
-            basic
-            closeIcon
-            size='small'
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={<Button>Watch Trailer</Button>}
-        >
-            <Modal.Content>
-                <ReactPlayer
-                    controls
-                    playing = {true}
-                    url={`https://youtu.be/${filterTrailer()}`}
-                />
-            </Modal.Content>
-            {/* <Modal.Actions>
+    if (filterTrailer()) {
+        return (
+            <Modal
+                basic
+                closeIcon
+                size='small'
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                open={open}
+                trigger={<Button>Watch Trailer</Button>}
+            >
+                <Modal.Content>
+                    <ReactPlayer
+                        controls
+                        playing={true}
+                        url={`https://youtu.be/${filterTrailer()}`}
+                    />
+                </Modal.Content>
+                {/* <Modal.Actions>
         <Button color="black" onClick={() => setOpen(false)}>
           Close
         </Button>
       </Modal.Actions> */}
-        </Modal>
-    );
+            </Modal>
+        );
+    }
+    return "";
 };
 
 
